a8de28b5554412076ed606339a0630107189603a
Matheus Rebola <m.cavalcantir@outlook.com>
2025-08-31 10:57:23 -0300

    feat: adição do serviço de autenticação

---
+++ service/autenticacao/src/test/java/iasd/coralzinho/autenticacao/AutenticacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package iasd.coralzinho.autenticacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutenticacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
